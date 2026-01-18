b83d1024a95ccd4b92c08f8f42f43a828a66f99f
Yuvanesh Sankar <yuvanesh.skv@gmail.com>
2026-01-19 01:56:08 +0530

    refactor->planning to use unsloth for finetuning the model instead of using peft in my low spec lap....so stopped the training midway

--- src/train_sft.py
+++ src/train_sft.py
@@ -1,7 +1,7 @@
 import torch
 from datasets import load_dataset
 from peft import LoraConfig
-from transformers import AutoModelForCausalLM, AutoTokenizer , BitsandBytesConfig , TrainingArguments
+from transformers import AutoModelForCausalLM, AutoTokenizer , BitsAndBytesConfig , TrainingArguments
 from trl import SFTTrainer,SFTConfig
 model_name="TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 dataset_name = "tatsu-lab/alpaca"  # 52k instructions
@@ -10,13 +10,13 @@ bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,  #does quantization on the qauntization constants to reduce memory usage
     bnb_4bit_quant_type="nf4",  #normal float 4 ->datatype like fp4
-    bnb_4bit_compute_dtype=torch.bfloat16  #what datatype to use while computation
+    bnb_4bit_compute_dtype=torch.float16  #what datatype to use while computation
 )
 model=AutoModelForCausalLM.from_pretrained(
     model_name,
     quantization_config=bnb_config,
     device_map="auto",        #Automatically assigns model layers to available devices ->like only gpu or cpu+gpu or multiple gpus...
-    torch_dtype=torch.bfloat16
+    torch_dtype=torch.float16
 )
 
 
@@ -32,7 +32,7 @@ peft_config = LoraConfig(
     lora_alpha=32, #Scaling factor....now alpha/r=>32/16=2..so this scales the lora updates before adding them to the original weights
     lora_dropout=0.05,
     bias="none", #no need to update bias terms during training
-    task_type="CAUSAL_LM" #causal language modeling task(basically auto regressive language modeling)
+    task_type="CAUSAL_LM" ,#causal language modeling task(basically auto regressive language modeling)
     target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"]
     #These are the layers within the model that will be adapted using LoRA.
     #query , key , value , ouptput projection layers and feed forward network layers
@@ -55,12 +55,11 @@ sft_config = SFTConfig(
     optim="paged_adamw_8bit",  #This is a memory-optimized optimizer from bitsandbytes
     logging_steps=10,  #just logs training metrics every 10 steps
     save_steps=500,  #saves model checkpoint every 500 steps
-    evaluation_strategy="steps",   #run evaluation at each save step not only at end of epoch
     eval_steps=500,  #run evaluation every 500 steps
     max_seq_length=2048,  # TinyLlama max
     packing=True,  # This is a TRL-specific optimization....minmizes padding tokens in batches
-    fp16=False,  # Use bfloat16 above
-    bf16=True,
+    fp16=True,  # Use bfloat16 above
+    bf16=False,
     # report_to="wandb",  # Optional: wandb login
     remove_unused_columns=False,
 )
